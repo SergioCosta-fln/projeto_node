db64b930b15c0374724d308d20de72623ef71b2f
Sergio Costa <sergio.costa@wattei.com.br>
2021-06-11 16:54:43 -0300

    Modulo 4 - Aula 14 - Protegendo Rotas (2/2)

--- helpers.js
+++ helpers.js
@@ -1,10 +1,11 @@
 exports.defaultPageTitle = 'Projeto Node';
 
 exports.menu = [
-    {name:'Home', slug:'/'},
-    {name:'Login', slug:'/users/login'},
-    {name:'Cadastro', slug:'/users/register'},
-    {name:'Adicionar Post', slug:'/post/add'}
+    {name:'Home', slug:'/', guest:true, logged:true},
+    {name:'Login', slug:'/users/login', guest:true, logged:false},
+    {name:'Cadastro', slug:'/users/register', guest:false, logged:false},
+    {name:'Adicionar Post', slug:'/post/add', guest:false, logged:true},
+    {name:'Sair', slug:'/users/logout', guest:false, logged:true}
 ];
 
 exports.submenu = [
